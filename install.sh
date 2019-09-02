96c4d16dba2825696c2e91dd613230f7c55cf92d
Benjamin Noufel <benjamin.noufel@gmail.com>
2019-09-02 06:38:51 +0200

    fix links

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ ${HOME}/.brew/bin/brew install cask
 ${HOME}/.brew/bin/brew cask reinstall $(cat .cask)
 
 # Linkage
-ln -s ${EXEC}/.zshrc ${HOME}/
-ln -s ${EXEC}/.gitconfig ${HOME}/
-ln -s ${EXEC}/.gitignore_global ${HOME}/
-ln -s ${EXEC}/.vimrc  ${HOME}/
+ln -s ${EXEC}/.zshrc ${HOME}
+ln -s ${EXEC}/.gitconfig ${HOME}
+ln -s ${EXEC}/.gitignore_global ${HOME}
+ln -s ${EXEC}/.vimrc  ${HOME}
 chmod +x ${EXEC}/.osxconfig
 ./.osxconfig
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
